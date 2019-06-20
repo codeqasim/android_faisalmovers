903bd9130727c0128b6a542bcd7ee76f3f670f6e
symmalik <symmalik99@gmail.com>
2019-06-20 03:36:31 -0700

    update

--- Bingoo_ws_format/app/src/main/java/com/faisalmovers/travels/bus/Forgotpassword.java
+++ Bingoo_ws_format/app/src/main/java/com/faisalmovers/travels/bus/Forgotpassword.java
@@ -3,6 +3,7 @@ package com.faisalmovers.travels.bus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class Forgotpassword extends AppCompatActivity {
@@ -10,10 +11,12 @@ public class Forgotpassword extends AppCompatActivity {
 
 
     ProgressBar progressBar2;
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpassword);
+
 
 
         progressBar2 = findViewById(R.id.progressBar2);
@@ -24,5 +27,10 @@ public class Forgotpassword extends AppCompatActivity {
     public  void summitpassword(View view)
     {
 
+    }
+
+    public  void backscreen(View view)
+    {
+        finish();
     }
 }
